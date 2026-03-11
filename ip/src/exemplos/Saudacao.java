2268064cd2249e1ea81f6db16747a32399fddd30
Matheus Andrade <matheus61489206@alunos.sc.senac.br>
2026-03-11 20:55:18 -0300

    Primeiro commit

---
+++ ip/src/exemplos/Saudacao.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Saudacao {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Informe seu nome:");
+		String nome = teclado.nextLine();
+		
+		System.out.println("Olá " + nome + ",seja bem vindo!!");
+
+	}
+
+}
---
+++ ip/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author matheus61489206
+ *
+ */
+module ip {
+}
