01397c0c1e29779cb72eeea1086450d965ae78a4
matheus-de-andrade <matheusdeandradesc@gmail.com>
2026-04-15 09:31:17 -0300

    .

--- ip/src/exercicio_array_matriz/Ex04.java
+++ ip/src/exercicio_array_matriz/Ex04.java
@@ -9,20 +9,30 @@ public class Ex04 {
 		int[] nums = new int[10];
 		int i = 0;
 		int numMaior = -1;
+		int indMaior = 0;
+		long numMenor = 10000000000L;
+		int indMenor = 0;
 		while(i < nums.length) {
 			System.out.print("Digite o " + (i + 1) + "° número positivo: ");
 			nums[i] = teclado.nextInt();
 			if(nums[i] > 0) {
-				i++;
 				if (numMaior < nums[i]) {
 					numMaior = nums[i];
+					indMaior = i;
 				}
+				if (numMenor > nums[i]) {
+					numMenor = nums[i];
+					indMenor = i;
+				}
+				i++;
 			}
 			else {
 				System.out.print("Opção inválida, tente de novo");
 			}
+			System.out.print("\nO número maior é: " + numMaior + " aparencendo no indice " + indMaior);
+			System.out.print("\nO número menor é: " + numMenor + " aparencendo no indice " + indMenor);
 		}
-
+		teclado.close();
 	}
 
 }
